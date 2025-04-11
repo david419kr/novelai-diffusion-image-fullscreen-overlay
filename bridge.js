ac855f70cf874ab27337155a0279e0046b83b2b8
david419kr <david419kr@gmail.com>
2025-04-11 12:21:28 +0900

    remove auto generation due to potential TOS violation

--- bridge.js
+++
@@ -1,4 +0,0 @@
-const script = document.createElement('script');
-script.src = chrome.runtime.getURL('injector.js');
-(document.head || document.documentElement).appendChild(script);
-script.onload = () => script.remove();
